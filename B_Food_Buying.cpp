4c16839afa27b23e37ee9d152d0c6cfdb95c0f01
santhoshsks <santhoshkoh@gmail.com>
2023-08-31 21:04:14 +0530

    done

---
+++ B_Food_Buying.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        long x=long(n*1.11111111111);
+        cout<<x<<'\n';
+    }
+    return 0;
+}
