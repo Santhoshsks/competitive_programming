9c8090addc5b454f1aa0d4d3c79ed378d44f7ab5
santhoshsks <santhoshkoh@gmail.com>
2023-09-10 15:28:54 +0530

    done

--- functions/power_function.cpp
+++ functions/power_function.cpp
@@ -1,16 +1,23 @@
-class Solution {
-public:
-    double myPow(double x, int n) {
-        if(n==0) return 1;
-        if(n<0) {
-            n = abs(n);
-            x = 1/x;
-        }
-        if(n%2==0){
-            return myPow(x*x, n/2);
-        }
-        else{
-            return x*myPow(x, n-1);
+#include<bits/stdc++.h>
+using namespace std;
+
+double Pow(double x, int n) {
+    double ans = 1.0;
+    long long n1 = n;
+    if (n < 0) n1 = -1 * n1;
+    while (n1) {
+        if (n1 % 2) {
+            ans = ans * x;
+            n1 = n1 - 1;
+        } else {
+            x = x * x;
+            n1 = n1 / 2;
         }
     }
-};
+    if (n < 0) ans = (double)(1.0) / (double)(ans);
+    return ans;
+}
+
+int main() {
+  cout << Pow(2.3, 2) << endl;
+}
