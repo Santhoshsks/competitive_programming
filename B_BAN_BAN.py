a1b8c97f43655161d9d3f4a07399ab3af8994f3c
santhoshsks <santhoshkoh@gmail.com>
2023-09-11 22:54:30 +0530

    DONE

--- B_BAN_BAN.py
+++ B_BAN_BAN.py
@@ -2,16 +2,16 @@ for _ in range(int(input())):
     n = int(input())
     if n == 1:
         print(1)
-        print(1,2)
+        print(1,3)
     else:
         if not (n % 2):
-            a = 2
-            b = 6
+            a = 1
+            b = 9
             print(n // 2)
             for i in range(n // 2):
                 print(a, b)
-                a = b + 2
-                b = b + 6
+                a = b + 1
+                b = b + 9
         else:
             print((n // 2) + 1)
             print(1,2)
