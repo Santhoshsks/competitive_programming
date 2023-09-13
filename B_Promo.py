07bc11cc4cf59bd84626ee7bd8274992ad713e36
santhoshsks <santhoshkoh@gmail.com>
2023-09-13 20:36:49 +0530

    done

--- B_Promo.py
+++ B_Promo.py
@@ -4,10 +4,13 @@ for _ in range(q):
     maxi, mini = map(int, input().split())
     l1 = l[:maxi]
     for i in range(maxi, len(l)):
-        me = min(l1)
-        mi = l1.index(me)
-        if l[i] > me:
-            l1[mi] = l[i]
+        m = l[i]
+        for j in range(maxi):
+            if m > l1[j]:
+                temp = l1[j]
+                l1[j] = m
+                m = temp
+        
     l1.sort()
     l1 = l1[:mini]
     print(sum(l1))
