ba396b9feb2f128f894c9215f727ad2422795ae2
santhoshsks <santhoshkoh@gmail.com>
2023-09-16 20:51:55 +0530

    done

---
+++ A_Buttons.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    a, b, c = map(int,input().split())
+    if a == b:
+        if (a + b + c) % 2:
+            print("First")
+        else:
+            print("Second")
+
+    elif a > b:
+        print("First")
+    
+    else:
+        print("Second")
