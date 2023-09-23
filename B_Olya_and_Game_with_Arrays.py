968b435a55008a563d262b46ae703e9e30494d1b
santhoshsks <santhoshkoh@gmail.com>
2023-09-23 21:14:34 +0530

    done

---
+++ B_Olya_and_Game_with_Arrays.py
@@ -0,0 +1,19 @@
+for _ in range(int(input())):
+    smallest = float('inf')
+    sec_smallest = float('inf')
+    ans = 0
+    list_len = int(input())
+    if list_len == 1:
+        list(map(int, input().split()))
+        print(min(list(map(int, input().split()))))
+    else:
+        for _ in range(list_len):
+            n = int(input())
+            arr = list(map(int, input().split()))
+            arr.sort()
+            if arr[0] < smallest:
+                smallest = arr[0]
+            if arr[1] < sec_smallest:
+                sec_smallest = arr[1]
+            ans += arr[1]
+        print(ans + smallest - sec_smallest)
