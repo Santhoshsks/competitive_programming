aa55073753ac7e25c5bc15add79207af7eed01f8
santhoshsks <santhoshkoh@gmail.com>
2023-09-26 21:16:16 +0530

    done

--- A_Beautiful_Year.cpp
+++ A_Beautiful_Year.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    n++;
     while (true) {
         string s = to_string(n);
         set<char> set;
         for (int i = 0; i < 4; i++) {
-            
+            set.insert(s[i]);
         }
+        if (set.size() == 4) {cout << s;break;}
+        else n = stoi(s) + 1;
     }
 }
