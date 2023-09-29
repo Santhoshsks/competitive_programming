50a9e51dd60fb665a53b51f7695709cfbd09c414
santhoshsks <santhoshkoh@gmail.com>
2023-09-29 17:47:55 +0530

    done

---
+++ A_Stones_on_the_Table.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int fin = 1;
+    int ini = 0;
+    int ans = 0;
+    if (n == 1) {cout << ans;} 
+    else {
+        while (fin < n) {
+            if (s[ini] == s[fin])  {
+                
+            }
+        }
+    }
+}
