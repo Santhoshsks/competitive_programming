942b1b93f5c83d97fc849eb50128c77018f837ce
santhoshsks <santhoshkoh@gmail.com>
2023-09-29 18:39:40 +0530

    done

--- A_Panoramix_s_Prediction.cpp
+++ A_Panoramix_s_Prediction.cpp
@@ -1,15 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool prime(int n) {
+    if (n == 3) return true;
+    for (int ind = 2; ind <= sqrt(n); ind++) {
+        if (n % ind == 0) return false;
+    }
+    return true;
+}
 
 int main() {
     int a,b;
     cin >> a;
     cin >> b;
-    bool t = false;
-    for (int i = 2; i <= sqrt(b); i++) {
-        if (b % i == 0) {
-            t = true;
-        }
+    bool t = true;
+    for(int i=a+1; i<=b; i++) {
+    if (prime(i))
+    {
+        if (i==b) t = false;else break;
+    }
     }
     if (t) cout << "NO";
     else cout << "YES";
