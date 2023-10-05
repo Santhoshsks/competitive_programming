661c848980b618422a1bf4faf85fc2aae8a5e6cd
santhoshsks <santhoshkoh@gmail.com>
2023-10-05 20:45:44 +0530

    done

---
+++ A_I_love_username.py
@@ -0,0 +1,14 @@
+n = int(input())
+l = list(map(int, input().split()))
+mini, maxi = l[0], l[0]
+ans = 0
+
+for i in range(1,n):
+    if l[i] < mini:
+        ans += 1
+        mini = l[i]
+    elif l[i] > maxi:
+        ans += 1
+        maxi = l[i]
+
+print(ans)
