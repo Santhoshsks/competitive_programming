dccb9c2d0aa34e45e22c30330957ce83dfdbf39e
santhoshsks <santhoshkoh@gmail.com>
2023-10-05 22:26:03 +0530

    done

--- t.py
+++ t.py
@@ -1,6 +1,18 @@
-result = 0
-for i in range(26, 0, -1):
-    result += 1 / i
+def min_copper_coins(n):
+    if n == 0:
+        return 0
+    dp = [float('inf')] * (n + 1)
+    dp[0] = 0
+    for i in range(1, n + 1):
+        dp[i] = min(dp[i], dp[i - 1] + 1)
+        if i >= 2:
+            dp[i] = min(dp[i], dp[i - 2] + 1)
+        if i >= 4:
+            dp[i] = min(dp[i], dp[i - 4] + 1)
+    return dp[n]
 
-result *= 26
-print("ans:", result)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    result = min_copper_coins(n)
+    print(result)
