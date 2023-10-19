e285fa8f60e6394dfe69e1b6346141884e095f6b
santhoshsks <santhoshkoh@gmail.com>
2023-10-19 20:24:54 +0530

    done

--- A_Parallelepiped.java
+++ A_Parallelepiped.java
@@ -1,30 +1,12 @@
 import java.util.Scanner;
 
 public class A_Parallelepiped {
-    public static int gcd(int a, int b) {
-        int i = 1;
-        if (a < b) 
-            i = a;
-        else 
-            i = b;
-
-        for(;i > 1; i--) {
-            if (a % i == 0 && b % i == 0) {
-                return i;
-            }
-        }
-        return 1;
-    }
-
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        int gcdOfab = gcd(a,b);
-        int gcdOfbc = b / gcdOfab;
-        int gcdOfac = c / gcdOfbc;
-        
-        System.out.print(4 * (gcdOfab + gcdOfbc + gcdOfac));
+        int ans = (int)(4 * (Math.sqrt((a*b)/c) + Math.sqrt((c*b)/a) + Math.sqrt((c*a)/b)));
+        System.out.print(ans);
     }
 }
