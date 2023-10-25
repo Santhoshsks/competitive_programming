ec57b74f637085a90a5611a22afbe6cae4d3ba26
santhoshsks <santhoshkoh@gmail.com>
2023-10-25 21:55:36 +0530

    done

--- B_Xenia_and_Ringroad.java
+++ B_Xenia_and_Ringroad.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
 public class B_Xenia_and_Ringroad {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        sc.nextLine();
-        int temp = sc.nextInt();
-        int prev = temp;
-        int ans = temp;
-        for (int i = 0; i < m - 1; i++) {
-            temp = sc.nextInt();
-            if (temp < prev) {
-                ans += (n - prev) + temp;
-                prev = temp;
+        int prev = 1; // Start at house 1
+        long ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            int ai = sc.nextInt();
+            if (ai >= prev) {
+                ans += ai - prev;
             } else {
-                ans += temp - prev;
-                prev = temp;
+                ans += n - prev + ai;
             }
+            prev = ai;
         }
+        
         sc.close();
-        System.out.println(ans - 1);        
+        System.out.println(ans);
     }
 }
