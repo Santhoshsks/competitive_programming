51751c4d58674b08f3a1990d4c6fc276c780fbe6
santhoshsks <santhoshkoh@gmail.com>
2023-10-26 19:36:25 +0530

    done

---
+++ A_Magic_Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string inp;
+    cin >> inp;
+    int n = inp.size();
+    for (int i = 0; i < n; i++) {
+        if (inp[i] == '1') {
+            if (i + 1 < n) {
+                if (inp[i + 1] == '1') {
+                    continue;
+                }
+                else if (inp[i + 1] == '4') {
+                    if (i + 2 < n) {
+                        if (inp[i + 2] == '4') {
+                            
+                    }
+                    }
+            }
+            }
+        }
+    }
+}
