ac8cbcac3f3c046631fbb85073ba6e9c27a3fe7b
santhoshsks <santhoshkoh@gmail.com>
2023-10-27 14:23:06 +0530

    done

---
+++ A_Translation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  string reversed_s = "";
+  for (int i = s.length() - 1; i >= 0; i--) {
+    reversed_s += s[i];
+  }
+
+  if (reversed_s == t) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
