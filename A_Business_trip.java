cc8d9ce809852518747166e027779ebbc465b01c
santhoshsks <santhoshkoh@gmail.com>
2023-11-01 18:59:41 +0530

    done

---
+++ A_Business_trip.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class A_Business_trip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();  
+        int[] a = new int[12];  
+
+        for (int i = 0; i < 12; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int result = minMonthsToWaterFlower(k, a);
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    public static int minMonthsToWaterFlower(int k, int[] a) {
+        Arrays.sort(a);
+        int temp = 0;
+        if (k == 0) return 0;
+        else {
+            for (int i = a.length - 1; i >= 0; i--) {
+                temp += a[i];
+                if (temp >= k) {
+                    return a.length - i;
+                }
+            }
+        }
+        return -1; 
+    }
+}
