472cd222c774cfcbe44954f7d2984f63459cb21e
santhoshsks <santhoshkoh@gmail.com>
2023-11-09 20:39:58 +0530

    done

--- B_Present_from_Lena.py
+++ B_Present_from_Lena.py
@@ -1,13 +1,20 @@
 n = int(input())
 
-for i in range(n + 2):
+spaces = " " * ((n + 1) * 2) 
+numbers = ""
+print(f"{spaces}{numbers}{spaces}", end="")
+
+for i in range(1, n + 2):
     spaces = " " * ((n + 1 - i) * 2) 
     numbers = " ".join(str(j) for j in range(i))
-    line = f"{spaces}{numbers}{numbers[:-1][::-1]}{spaces}"
-    print(line)
+    print(f"{spaces}{numbers}{numbers[:-1][::-1]}{spaces}")
+
 
 for i in range(n , -1, -1):
     spaces = " " * ((n + 1 - i) * 2) 
     numbers = " ".join(str(j) for j in range(i))
-    line = f"{spaces}{numbers}{numbers[:-1][::-1]}{spaces}"
-    print(line)
+    print(f"{spaces}{numbers}{numbers[:-1][::-1]}{spaces}")
+
+spaces = " " * ((n + 1) * 2) 
+numbers = ""
+print(f"{spaces}{numbers}{spaces}", end="")
