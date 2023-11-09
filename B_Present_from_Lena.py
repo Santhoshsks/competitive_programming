b52e269c4104561249682bea4ab0f8b10f03f29e
santhoshsks <santhoshkoh@gmail.com>
2023-11-09 21:20:32 +0530

    done

--- B_Present_from_Lena.py
+++ B_Present_from_Lena.py
@@ -1,20 +1,16 @@
 n = int(input())
 
-spaces = " " * ((n + 1) * 2) 
-numbers = ""
-print(f"{spaces}{numbers}{spaces}", end="")
 
 for i in range(1, n + 2):
     spaces = " " * ((n + 1 - i) * 2) 
     numbers = " ".join(str(j) for j in range(i))
-    print(f"{spaces}{numbers}{numbers[:-1][::-1]}{spaces}")
-
+    print(f"{spaces}{numbers}{numbers[:-1][::-1]}")
 
 for i in range(n , -1, -1):
     spaces = " " * ((n + 1 - i) * 2) 
     numbers = " ".join(str(j) for j in range(i))
-    print(f"{spaces}{numbers}{numbers[:-1][::-1]}{spaces}")
+    if i == 1:
+        print(f"{spaces}{numbers}{numbers[:-1][::-1]}",end = "")
+    else:
+        print(f"{spaces}{numbers}{numbers[:-1][::-1]}")
 
-spaces = " " * ((n + 1) * 2) 
-numbers = ""
-print(f"{spaces}{numbers}{spaces}", end="")
