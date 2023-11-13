91bb90d9af6be13b712fae02f91b80ae68944b00
santhoshsks <santhoshkoh@gmail.com>
2023-11-13 14:27:33 +0530

    done

--- B_Airport.cpp
+++ B_Airport.cpp
@@ -1,32 +1,35 @@
-#include <iostream>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n, m;
     cin >> n >> m;
 
-    int a[m];
+    vector<int> a(m);
+    vector<int> b(m);
+
     for (int i = 0; i < m; ++i) {
         cin >> a[i];
     }
 
-    sort(a, a + m);
+    sort(a.begin(), a.end());
+    b = a;
 
     int minSum = 0;
     int maxSum = 0;
 
     for (int i = 0; i < n; ++i) {
-        minSum += a[0];
-        --a[0];
-
-        if (a[0] == 0) {
-            sort(a, a + m);
+        minSum += a.front();
+        --a.front(); 
+        if (a.front() == 0) {
+            sort(a.begin(), a.end());
+            a.erase(a.begin());
         }
 
-        maxSum += a[m - 1];
-        --a[m - 1];
+        maxSum += b.back();
+        --b.back();
+        sort(b.begin(), b.end());
+
     }
 
     cout << maxSum << " " << minSum << endl;
