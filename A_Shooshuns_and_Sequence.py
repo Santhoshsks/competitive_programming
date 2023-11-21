026a0ce045498f7281372d3acd26ab8aff633099
santhoshsks <santhoshkoh@gmail.com>
2023-11-21 23:32:21 +0530

    done

--- A_Shooshuns_and_Sequence.py
+++ A_Shooshuns_and_Sequence.py
@@ -1,16 +1,19 @@
-def min_operations_to_same_numbers(n, k, sequence,count,ans):
-    if count == 0 and sum(sequence) / n != sequence[0]:
-        return -1
-    elif sum(sequence) / n == sequence[0]:
-        return ans
-    else:
-        a = sequence[k]
-        b = sequence[1:]
-        b.append(a)
-        return min_operations_to_same_numbers(n,k,b,count - 1,ans + 1)
-
 n, k = map(int, input().split())
 sequence = list(map(int, input().split()))
 
-result = min_operations_to_same_numbers(n, k - 1, sequence,n,0)
-print(result)
+ans = 0
+flag = 0
+
+for i in range(n):
+    if all(element == sequence[0] for element in sequence):
+        flag = 1
+    else:
+        sequence.append(sequence[k - 1])
+        sequence.pop(0)
+        ans += 1
+
+if flag == 1:
+    print(ans)
+
+else:
+    print(-1)
